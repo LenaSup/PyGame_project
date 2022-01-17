32e55b570a7e9b1c60536b14dafaafafbc8a0672
Sturostina <The_long_duck@mail.ru>
2022-01-17 22:01:08 +0300

    заготовка для стартового окна

---
+++ main.py
@@ -0,0 +1,70 @@
+import pygame
+import sys
+import os
+import random
+
+
+def load_image(neme, color_key=None):   # ---
+    fullname = os.path.join('data', neme)
+
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error as massage:
+        raise SystemExit(massage)
+
+    if color_key is not None:
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+class AchievementBut(pygame.sprite.Sprite):
+    def __init__(self, group, size):
+        super().__init__(group)
+        pass
+
+
+class ExitBut(pygame.sprite.Sprite):
+    def __init__(self, group, size):
+        super().__init__(group)
+        pass
+
+
+class InfoBut(pygame.sprite.Sprite):
+    def __init__(self, group, size):
+        super().__init__(group)
+        pass
+
+
+class LearningBut(pygame.sprite.Sprite):
+    def __init__(self, group, size):
+        super().__init__(group)
+        pass
+
+
+class StartMenu:    # стартовое меню
+    def __init__(self, start_buttons_sprites, size):
+        self.start_buttons_sprites = start_buttons_sprites
+        pass
+
+    def start_menu_display(self):
+        runnind = True
+        while runnind:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    runnind = False
+            pass
+
+
+def main():
+    pygame.init()
+    size = width, height = 1280, 720
+    screen = pygame.display.set_mode(size)
+    pass
+
+
+if __name__ == '__main__':
+    main()
