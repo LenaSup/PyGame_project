ef4ccd6ce561445433992e1a1f54b3432ad1c1b3
Sturostina <The_long_duck@mail.ru>
2022-01-18 17:19:00 +0300

    прописка окна, отоброжение фона и коменты

--- main.py
+++ main.py
@@ -15,48 +15,54 @@ def load_image(neme, color_key=None):   # Вся графика хронится
     return image
 
 
-class AchievementBut(pygame.sprite.Sprite):
+class Inscription(pygame.sprite.Sprite):    # Декоротивная тобличка с назвением игры
     def __init__(self, group, size):
         super().__init__(group)
         pass
 
 
-class ExitBut(pygame.sprite.Sprite):
+class AchievementBut(pygame.sprite.Sprite):     # Кнобка в меню
     def __init__(self, group, size):
         super().__init__(group)
         pass
 
 
-class InfoBut(pygame.sprite.Sprite):
+class ExitBut(pygame.sprite.Sprite):    # Кнобка в меню
     def __init__(self, group, size):
         super().__init__(group)
         pass
 
 
-class LearningBut(pygame.sprite.Sprite):
+class InfoBut(pygame.sprite.Sprite):    # Кнобка в меню
     def __init__(self, group, size):
         super().__init__(group)
         pass
 
 
-class MenuClouds(pygame.sprite.Sprite):
+class LearningBut(pygame.sprite.Sprite):    # Кнобка в меню
     def __init__(self, group, size):
+        super().__init__(group)
+        pass
+
+
+class MenuClouds(pygame.sprite.Sprite):     # Облока в меню (облока в основкой игре меньше
+    def __init__(self, group, size):        # и имеют другой диопозон кординат спавна)
         super().__init__(group)
         pass
 
 
 class StartMenu:    # стартовое меню
-    def __init__(self, start_menu_sprites, size):
+    def __init__(self, start_menu_sprites):
         self.start_menu_sprites = start_menu_sprites
-        pass
 
-    def start_menu_display(self, screen):
+    def start_menu_display(self, screen, size):
         while True:
-            screen.blit(load_image('start_menu_background.png'), (0, 0))
+            background = pygame.transform.scale(load_image('start_menu_background.png'), size)
+            screen.blit(background, (0, 0))
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    sys.exit()
+                    sys.exit()  # Выхлд из игры
             self.start_menu_sprites.draw(screen)
             pygame.display.flip()
 
@@ -66,10 +72,10 @@ def main():
     size = width, height = 1280, 720
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption('First board')
-    # ----
-    start_menu_sprites = pygame.sprite.Group()
-    start_menu = StartMenu(start_menu_sprites, size)    # Создание обекта стартового миню
-    start_menu.start_menu_display(screen)   # Вывод меню при включение (до основного цикла игры)
+    # ----  Дополненая часть к прописаному мэйно(до игрового цикла)
+    start_menu_sprites = pygame.sprite.Group()  # Эта група спрайтов отображаемых в стартовом меню
+    start_menu = StartMenu(start_menu_sprites)    # Создание обекта стартового миню
+    start_menu.start_menu_display(screen, size)   # Вывод меню при включение
     # ----
     running = True
     while running:
