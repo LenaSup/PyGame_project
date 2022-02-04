e3e16cb92956fd14594192b2e856492885f10e39
Pechenka106 <94699772+Pechenka106@users.noreply.github.com>
2022-02-04 17:55:07 +0300

    Финальная версия

--- main.py
+++ main.py
@@ -904,8 +904,8 @@ def find_key(dictionary, needle):  # найти башню которая пер
 
 def terminate():  # закрытие программы
     db.close()
-    pygame.quit()
     sys.exit()
+    pygame.quit()
 
 
 def main_menu(screen):
@@ -974,6 +974,12 @@ def load_menu(my_board, screen, enemy_types, towers_types):
     animated_towers = pygame.USEREVENT + 4
     enemy_animation = pygame.USEREVENT + 5
     move_bullets = pygame.USEREVENT + 6
+    pygame.time.set_timer(move_enemy, 30)
+    pygame.time.set_timer(spawn_enemy, waves[current_wave][1])
+    pygame.time.set_timer(time_is_passing, 1000)
+    pygame.time.set_timer(animated_towers, 150)
+    pygame.time.set_timer(enemy_animation, 50)
+    pygame.time.set_timer(move_bullets, 1)
     time_level = 0
 
     n_enemies = [0 for _ in range(len(enemy_types))]
@@ -1007,8 +1013,8 @@ def main():
     # создание окна
     pygame.init()
     screen = pygame.display.set_mode(size)
-    pygame.display.set_caption('First board')
-    pygame.display.set_icon(load_image('icon.png'))
+    pygame.display.set_caption('Castle defense')
+    pygame.display.set_icon(pygame.transform.scale(load_image('icon.jpg'), (60, 60)))
 
     # создания поля
     my_board = Board(screen, 14, 6)
@@ -1020,6 +1026,11 @@ def main():
     current_level = main_menu(screen)
     playing_field = pygame.transform.scale(load_image(f'background_{current_level + 1}.png'), size)
     print(f'background_{current_level}.png')
+
+    f = pygame.font.Font('7X7PixelizedRegular.ttf', 60)
+    pause = pygame.transform.scale(load_image('pause_empty_field.png'), size)
+    text_1 = [f.render('Продолжить', False, (51, 51, 102)), (400, 230)]
+    text_2 = [f.render('Главное меню', False, (51, 51, 102)), (358, 340)]
     #
     clickable_interface_elements = pygame.sprite.Group()
     UpgradeBtn(clickable_interface_elements, size)
@@ -1062,8 +1073,7 @@ def main():
 
     print_radius = (0, 0), 0
     is_load = False
-    flag = False
-    pause = False
+    is_pause = False
     running = True
 
     while running:
@@ -1078,20 +1088,8 @@ def main():
                 level, start_pos = generate_level(lvl, 80)
                 enemy_default_settings = (start_pos[0] * 80 + my_board.top,
                                           start_pos[1] * 80 + my_board.cell_size // 4 + my_board.bot)
-                pygame.time.set_timer(move_enemy, 0)
-                pygame.time.set_timer(spawn_enemy, 0)
-                pygame.time.set_timer(time_is_passing, 0)
-                pygame.time.set_timer(animated_towers, 0)
-                pygame.time.set_timer(enemy_animation, 0)
-                pygame.time.set_timer(move_bullets, 0)
-                pygame.time.set_timer(move_enemy, 30)
-                pygame.time.set_timer(spawn_enemy, waves[current_wave][1])
-                pygame.time.set_timer(time_is_passing, 1000)
-                pygame.time.set_timer(animated_towers, 150)
-                pygame.time.set_timer(enemy_animation, 50)
-                pygame.time.set_timer(move_bullets, 1)
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                pause = True
+                is_pause = True
             if event.type == pygame.MOUSEMOTION:
                 cell, pos = my_board.get_cell(event.pos)
                 if cell.name == 'Building_cell' and cell.tower != None:
@@ -1169,18 +1167,6 @@ def main():
             level, start_pos = generate_level(lvl, 80)
             enemy_default_settings = (start_pos[0] * 80 + my_board.top,
                                       start_pos[1] * 80 + my_board.cell_size // 4 + my_board.bot)
-            pygame.time.set_timer(move_enemy, 0)
-            pygame.time.set_timer(spawn_enemy, 0)
-            pygame.time.set_timer(time_is_passing, 0)
-            pygame.time.set_timer(animated_towers, 0)
-            pygame.time.set_timer(enemy_animation, 0)
-            pygame.time.set_timer(move_bullets, 0)
-            pygame.time.set_timer(move_enemy, 30)
-            pygame.time.set_timer(spawn_enemy, waves[current_wave][1])
-            pygame.time.set_timer(time_is_passing, 1000)
-            pygame.time.set_timer(animated_towers, 150)
-            pygame.time.set_timer(enemy_animation, 50)
-            pygame.time.set_timer(move_bullets, 1)
         if castle_health <= 0:
             EndScreen(False, screen, time_level)
             current_level = load_menu(my_board, screen, enemy_types, towers_types)
@@ -1189,18 +1175,6 @@ def main():
             level, start_pos = generate_level(lvl, 80)
             enemy_default_settings = (start_pos[0] * 80 + my_board.top,
                                       start_pos[1] * 80 + my_board.cell_size // 4 + my_board.bot)
-            pygame.time.set_timer(move_enemy, 0)
-            pygame.time.set_timer(spawn_enemy, 0)
-            pygame.time.set_timer(time_is_passing, 0)
-            pygame.time.set_timer(animated_towers, 0)
-            pygame.time.set_timer(enemy_animation, 0)
-            pygame.time.set_timer(move_bullets, 0)
-            pygame.time.set_timer(move_enemy, 30)
-            pygame.time.set_timer(spawn_enemy, waves[current_wave][1])
-            pygame.time.set_timer(time_is_passing, 1000)
-            pygame.time.set_timer(animated_towers, 150)
-            pygame.time.set_timer(enemy_animation, 50)
-            pygame.time.set_timer(move_bullets, 1)
         # отрисовка
         screen.blit(background, (0, 0))  # Фон с небом
         screen.blit(playing_field, (0, 0))      # Игровое поле
@@ -1214,31 +1188,30 @@ def main():
         bullets.draw(screen)
         enemies.draw(screen)
         pygame.draw.circle(screen, 'white', *print_radius, 1)
-        while pause:
-            f = pygame.font.Font('7X7PixelizedRegular.ttf', 60)
-            pause = pygame.transform.scale(load_image('pause_empty_field.png'), size)
+        while is_pause:
             screen.blit(pause, (0, 0))
-            text_1 = [f.render('продолжить', False, (51, 51, 102)), (400, 230)]
             screen.blit(text_1[0], text_1[1])
-            text_2 = [f.render('главное меню', False, (51, 51, 102)), (358, 340)]
             screen.blit(text_2[0], text_2[1])
             pygame.display.flip()
             for eve in pygame.event.get():
                 if eve.type == pygame.QUIT:
                     terminate()
                 if eve.type == pygame.KEYDOWN and eve.key == pygame.K_SPACE:
-                    pause = False
+                    is_pause = False
                 if eve.type == pygame.MOUSEBUTTONDOWN:
                     pos = pygame.mouse.get_pos()
                     if pos[0] in range(text_1[1][0], text_1[1][0] + text_1[0].get_width()) \
                             and pos[1] in range(text_1[1][1], text_1[1][1] + text_1[0].get_height()):
-                        flag = False
+                        is_pause = False
                     if pos[0] in range(text_2[1][0], text_2[1][0] + text_2[0].get_width()) \
                             and pos[1] in range(text_2[1][1], text_2[1][1] + text_2[0].get_height()):
                         castle_health = 0
-                        flag = False
+                        is_pause = False
         pygame.display.flip()
+    exit()
+    sys.exit()
 
 
 if __name__ == '__main__':
     main()
+    terminate()
